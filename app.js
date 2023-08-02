33bf376df4abb2faa85b825951992fda1102e4c4
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-02 19:05:47 +0300

    Error handling in tour GET request - updated

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/api/v1/tours', (req, res) => {
 app.get('/api/v1/tours/:id', (req, res) => {
   const returnId = req.params.id * 1;
   const tour = toursData.find((el) => el.id === returnId);
-  if (returnId >= toursData.length) {
+  if (!tour) {
     return res.status(404).json({
       status: 'Error',
       message: `Tour with id = ${returnId} doesn't exist`,
