020c5f3c6827a7547c92aee47761a1f89d7868dc
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-06 20:58:36 +0300

    Mongoose connection init

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const DB = process.env.DATABASE_PATH.replace(
   process.env.DATABASE_PASSWORD,
 );
 
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+});
+
 const app = require('./app');
 // Server start
 
