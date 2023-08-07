bb49163036061e42a0b11728d7dce27f33c2351a
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-07 20:46:12 +0300

    Updated updateTour function with error handling

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -49,14 +49,27 @@ exports.addTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<UPDATED TOUR>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
+
 exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
