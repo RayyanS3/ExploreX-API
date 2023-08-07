6ff0a618d1365c9dae65fe02b7435719fcf89f30
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-07 20:12:40 +0300

    Updated addTour function - error handling

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,16 +16,24 @@ exports.getTour = (req, res) => {
   //   data: { tour },
   // });
 };
-exports.addTour = async (req, res) => {
-  const newTour = await Tour.create(req.body);
 
-  res.status(201).json({
-    status: 'Success',
-    data: {
-      tour: newTour,
-    },
-  });
+exports.addTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
+
 exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
