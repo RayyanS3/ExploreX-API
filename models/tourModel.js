b8c80794639e2173ea905ad4585623b4244a20b8
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-07 23:43:18 +0300

    Updated tour schema to fit new data structure

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = mongoose.Schema({
     type: String,
     required: [true, 'Tour must have name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour must have duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have maximum group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour must have difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Tour must have price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  coverImage: {
+    type: String,
+    required: [true, 'Tour must have a cover image identifier'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
