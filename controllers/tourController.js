1e64b94cbc6803c3d50392c855bc887a326cb1b9
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-08 19:02:00 +0300

    Selective tour fields returned functionality added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,6 +22,13 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // Selective fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
     const allTours = await query;
 
     res.status(200).json({
