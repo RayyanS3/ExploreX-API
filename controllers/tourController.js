73a0830292f114637ee8f29c42c55479f676528e
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-09 00:34:49 +0300

    Aggregate pipeline used to calculate tours statistics

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -100,3 +100,39 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getToursStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: {
+          ratingsAverage: { $gte: 4.5 },
+        },
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRatings: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats: stats,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,6 +8,8 @@ router
   .route('/top-5-tours')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getToursStats);
+
 router.route('/').get(tourController.getAllTours).post(tourController.addTour);
 
 router
