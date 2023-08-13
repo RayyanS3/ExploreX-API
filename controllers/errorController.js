e2956b8e8f8fb82998435e93cf8c60d1b283a5b0
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-14 00:57:15 +0300

    Error handling - error controller updated to display only operational errors in production enviornment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,10 +8,17 @@ const getErrorDev = (err, res) => {
 };
 
 const getErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log(err);
+    res.status(500).json({
+      status: 'error',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
