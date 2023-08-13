a7fcab224c9b73387c5aa04a1d0a94b04c509663
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-14 00:42:47 +0300

    Error handling - added error handling for invalid id in tour handlers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 // Top tours middleware
@@ -28,6 +29,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('Tour not found', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: { tour },
@@ -50,6 +56,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true,
   });
 
+  if (!tour) {
+    return next(new AppError('Tour not found', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -59,7 +69,12 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('Tour not found', 404));
+  }
+
   res.status(204).json({
     status: 'success',
     message: 'selected tour deleted',
