a1f1c8a8907bbda39d7f28964fa90f0fbbf14ab0
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-13 19:06:52 +0300

    Error handling - global middleware handler init

--- app.js
+++ app.js
@@ -20,11 +20,27 @@ app.use('/api/v1/users', userRouter);
 
 // Error handling all other routes - middleware
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Route to ${req.originalUrl} does not exist on server`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Route to ${req.originalUrl} does not exist on server`,
+  // });
+
+  const err = new Error(`Route to ${req.originalUrl} does not exist on server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// Global error hndling middleware
+app.use((err, req, res, next) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
-  next();
 });
 
 // Express app export
