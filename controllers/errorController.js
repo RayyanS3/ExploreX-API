bee74f89f4a46720721321481804761b738a5894
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-14 00:50:32 +0300

    Error handling - error controller updated to send differnt errors based on node enviornment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,26 @@
-module.exports = (err, req, res, next) => {
-  err.status = err.status || 'error';
-  err.statusCode = err.statusCode || 500;
+const getErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    err: err,
+    stack: err.stack,
+  });
+};
 
+const getErrorProd = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  if (process.env.NODE_ENV === 'development') {
+    getErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    getErrorProd(err, res);
+  }
 };
