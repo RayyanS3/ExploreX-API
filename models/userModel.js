500f360e544b4a3295ade671dcb40169f329b29d
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-14 21:18:09 +0300

    User schema updated

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,29 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: [true, 'Tour must have name'],
+    required: [true, 'Please give us your name'],
   },
   email: {
     type: String,
-    required: [true, 'Tour must have name'],
+    required: [true, 'Please provide an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  photo: {},
-  password: {},
-  passwordConfirm: {},
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please provide a password'],
+  },
 });
 
 const User = mongoose.model('User', userSchema);
