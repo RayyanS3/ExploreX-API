38ff6f860ae14096b618cbe8d6aa01207adedb65
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-14 18:58:33 +0300

    Cast error handling - invalid id field inputs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path} field: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const getErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -14,7 +21,7 @@ const getErrorProd = (err, res) => {
       message: err.message,
     });
   } else {
-    console.log(err);
+    // console.log(err);
     res.status(500).json({
       status: 'error',
     });
@@ -28,6 +35,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     getErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    getErrorProd(err, res);
+    let errorCopy = { ...err };
+    if (errorCopy.kind === 'ObjectId') {
+      errorCopy = handleCastErrorDB(errorCopy);
+    }
+    getErrorProd(errorCopy, res);
   }
 };
