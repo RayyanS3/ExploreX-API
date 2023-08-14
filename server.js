f41097f324bbaa90b8a18064e3667663b54c429d
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-14 20:05:26 +0300

    Unhandled rejection error handling

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ mongoose
 
 // Server start
 const port = 3000 || process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Server started on port 3000');
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(`${err.name}: ${err.message}`);
+  console.log('Unhandled rejection\n Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
