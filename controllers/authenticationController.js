7f28f13c862fa2cbb2d274cf4e62993fb9481dfc
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-20 16:30:30 -0400

    Added protected route to get all tours handler

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -49,3 +49,7 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  next();
+});
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
-
+const authController = require('../controllers/authenticationController');
 const router = express.Router();
 
-// Route-handlers
+// Main routes
+router
+  .route('/')
+  .get(authController.protect, tourController.getAllTours)
+  .post(tourController.addTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+// Secondary routes
 router
   .route('/top-5-tours')
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -11,14 +23,6 @@ router
 router.route('/tour-stats').get(tourController.getToursStats);
 
 router.route('/monthly-plans/:year').get(tourController.getMonthlyPlans);
-
-router.route('/').get(tourController.getAllTours).post(tourController.addTour);
-
-router
-  .route('/:id')
-  .get(tourController.getTour)
-  .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
 
 // Export module
 module.exports = router;
