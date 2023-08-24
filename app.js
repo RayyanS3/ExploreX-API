3da61eee45db5ac2746777c779bb90d9c32a4865
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-23 21:28:47 -0400

    Added helmet middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -11,11 +12,17 @@ const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-// Middleware declerations
+// Global Middleware declerations
+
+// Secure HTTP headers
+app.use(helmet());
+
+// Dev logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Request limiter
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -23,7 +30,10 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Express options
+app.use(express.json({ limit: '10kb' }));
+
+// Serve static files
 app.use(express.static('public'));
 
 app.use('/api/v1/tours', tourRouter);
