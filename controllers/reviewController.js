50c4922f7fc57dce5b1b363c5a428c070a85416f
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-24 19:20:14 -0400

    Updated create review handler

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -14,4 +14,12 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   next();
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {});
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    message: 'Successfully posted review',
+    review: newReview,
+  });
+});
