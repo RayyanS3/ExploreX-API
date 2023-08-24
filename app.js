fab45ff420e903383824030f4bb988b43f6995a7
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-23 21:25:31 -0400

    Added request limiter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // NPM/Native modules import
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -14,6 +15,14 @@ const userRouter = require('./routes/userRoutes');
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests, please try again later.',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static('public'));
 
