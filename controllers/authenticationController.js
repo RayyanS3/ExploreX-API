31572b066d1f67f59ada6dc978cb065fd3c11c0c
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-23 21:15:58 -0400

    Cookie response sent containing jwt token

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -15,6 +15,13 @@ const signToken = (id) => {
 const createUserToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(Date.now() + 90 * 86400 * 1000),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(statusCode).json({
     status: 'success',
     token,
