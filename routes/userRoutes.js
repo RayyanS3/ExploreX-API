8550642183d7684e5bc15215715fd8d534769a86
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-26 21:23:42 -0400

    Added global middleware to protect all user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,22 +7,18 @@ const authController = require('../controllers/authenticationController');
 // Route handlers
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
-router
-  .route('/me')
-  .get(authController.protect, userController.getMe, userController.getUser);
-
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
-router
-  .route('/updatePassword')
-  .patch(authController.protect, authController.resetPassword);
 
-router
-  .route('/updateUserData')
-  .patch(authController.protect, userController.updateMe);
-router
-  .route('/deleteUser')
-  .delete(authController.protect, userController.deleteMe);
+router.use(authController.protect);
+
+router.route('/me').get(userController.getMe, userController.getUser);
+
+router.route('/updatePassword').patch(authController.resetPassword);
+
+router.route('/updateUserData').patch(userController.updateMe);
+
+router.route('/deleteUser').delete(userController.deleteMe);
 
 router.route('/').get(userController.getAllUsers).post(userController.addUser);
 
