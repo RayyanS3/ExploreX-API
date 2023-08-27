7f9af0680ebf78178359e3f17daf19aa01eb6d1d
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-26 21:52:05 -0400

    Updated user model

--- models/userModel.js
+++ models/userModel.js
@@ -48,15 +48,15 @@ const userSchema = mongoose.Schema({
 });
 
 // Middlewares
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) {
+//     return next();
+//   }
 
-  this.password = await bcrypt.hash(this.password, 12);
+//   this.password = await bcrypt.hash(this.password, 12);
 
-  this.passwordConfirm = undefined;
-});
+//   this.passwordConfirm = undefined;
+// });
 
 userSchema.pre(/^find/, function (next) {
   this.find({ active: { $ne: false } });
