96abb1a90da58a95472c76e4f1b251690c7990e1
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-29 17:07:04 -0400

    Updated getOverview to populate all tours into individual cardsfrom DB

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,14 @@
-exports.getOverview = (req, res) => {
+const catchAsync = require('../utils/catchAsync');
+const Tour = require('../models/tourModel');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
