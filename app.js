2b427b3868b3d7479af57f0fbdca4ad16c7ad377
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-29 16:32:57 -0400

    Refactored client-side code to fit MVC formatting

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Router import
+const viewRouter = require('./routes/viewRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
@@ -46,25 +47,8 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 
 // Serve static files
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Rayyan Suhail',
-  });
-});
 
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    tour: 'The Forest Hiker',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/review', reviewRouter);
