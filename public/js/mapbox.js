29512ab65632f4d1e06835c2adf1e3fb7cc370b0
Rayyan Suhail <suhailr@mcmaster.ca>
2023-08-30 20:56:20 -0400

    Added description to markers - popups

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -9,6 +9,7 @@ mapboxgl.accessToken =
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/rayyan-03/cllyf9lvt01ik01magppd4y11',
+  scrollZoom: false,
 });
 
 const bounds = new mapboxgl.LngLatBounds();
@@ -17,11 +18,20 @@ locations.forEach((loc) => {
   const el = document.createElement('div');
   el.className = 'marker';
 
+  // Add markers
   new mapboxgl.Marker({
     element: el,
     anchor: 'bottom',
   })
     .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup info
+  new mapboxgl.Popup({
+    offset: 35,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>${loc.description}</p>`)
     .addTo(map);
 
   bounds.extend(loc.coordinates);
