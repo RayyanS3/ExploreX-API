5a34103f55cb7f84a7c0506c3f41c4efb4a47b3c
Rayyan Suhail <suhailr@mcmaster.ca>
2023-09-04 17:33:09 -0400

    Completed signup feature and added default user photo option

--- models/userModel.js
+++ models/userModel.js
@@ -16,7 +16,7 @@ const userSchema = mongoose.Schema({
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  photo: String,
+  photo: { type: String, default: 'default.jpg' },
   role: {
     type: String,
     default: 'user',
