1ac0f32157b0d8a1c8160c34609760f785413792
Rayyan Suhail <suhailr@mcmaster.ca>
2023-09-06 09:06:04 -0400

    Updated settings

--- app.js
+++ app.js
@@ -1,84 +1,40 @@
-// NPM/Native modules import
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const xxs = require('xss-clean');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const AppError = require('./utils/appError');
-const cookieParser = require('cookie-parser');
-const globalErrorHandler = require('./controllers/errorController');
-const compression = require('compression');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-const app = express();
-
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-
-// Router import
-const viewRouter = require('./routes/viewRoutes');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes');
-
-// Global Middleware declerations
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(helmet());
-
-app.use((req, res, next) => {
-  res.setHeader(
-    'Content-Security-Policy',
-    "script-src 'self' https://cdnjs.cloudflare.com",
-  );
-  next();
+process.on('uncaughtException', (err) => {
+  console.log('Unhandled exception - Shutting down...');
+  console.log(`${err.name}: ${err.message}`);
+  process.exit(1);
 });
 
-// Data sanitization middlewares
-app.use(xxs());
-app.use(mongoSanitize());
-app.use(compression());
+dotenv.config({ path: './config.env' });
 
-// Dev logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+const index = require('./index');
 
-// Request limiter
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests, please try again later.',
-});
-app.use('/api', limiter);
+const DB = process.env.DATABASE_PATH.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 
-// Express options
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-app.use(cookieParser());
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB CONNECTION SUCCESSFUL'));
 
-app.use((req, res, next) => {
-  // console.log(req.cookies);
-  next();
+// Server start
+const port = 8000 || process.env.PORT;
+const server = index.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
 
-// Serve static files
-
-app.use('/', viewRouter);
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/review', reviewRouter);
-
-// Error handling all other routes - middleware
-app.all('*', (req, res, next) => {
-  next(
-    new AppError(`Route to ${req.originalUrl} does not exist on server`, 404),
-  );
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  console.log(`${err.name}: ${err.message}`);
+  console.log('Unhandled rejection - Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
-
-// Global error handling middleware
-app.use(globalErrorHandler);
-
-// Express app export
-module.exports = app;
---
+++ index.js
@@ -0,0 +1,79 @@
+// NPM/Native modules import
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const xxs = require('xss-clean');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const AppError = require('./utils/appError');
+const cookieParser = require('cookie-parser');
+const globalErrorHandler = require('./controllers/errorController');
+const compression = require('compression');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Router import
+const viewRouter = require('./routes/viewRoutes');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+
+// Global Middleware declerations
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(helmet());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "script-src 'self' https://cdnjs.cloudflare.com",
+  );
+  next();
+});
+
+// Data sanitization middlewares
+app.use(xxs());
+app.use(mongoSanitize());
+app.use(compression());
+
+// Dev logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Request limiter
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests, please try again later.',
+});
+app.use('/api', limiter);
+
+// Express options
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Serve static files
+
+app.use('/', viewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/review', reviewRouter);
+
+// Error handling all other routes - middleware
+app.all('*', (req, res, next) => {
+  next(
+    new AppError(`Route to ${req.originalUrl} does not exist on server`, 404),
+  );
+});
+
+// Global error handling middleware
+app.use(globalErrorHandler);
+
+// Express app export
+module.exports = app;
